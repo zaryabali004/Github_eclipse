dd8a9a969cb749aa6cbfc657b80d1f3302f37120
Ali <zaryaba941@gmail.com>
2024-04-14 14:02:30 +0200

    Commit 1 para clase 1 y 2

---
+++ Github_eclipse/src/clases/clase1.java
@@ -0,0 +1,9 @@
+package clases;
+
+public class clase1 {
+	public static void main(String[] args) {
+		System.out.println("Primera instantánea");
+	}
+	
+
+}
---
+++ Github_eclipse/src/clases/clase2.java
@@ -0,0 +1,8 @@
+package clases;
+
+public class clase2 {
+	public static void main(String[] args) {
+		System.out.println("Primera instantánea");
+	}
+
+}
